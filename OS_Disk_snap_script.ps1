b196e9bb7110b8476a434cf7370b435ffee86c30
kiranent2 <83274675+kiranent2@users.noreply.github.com>
2021-04-28 12:16:11 +0530

    Add files via upload

--- OS_Disk_snap_script.ps1
+++ OS_Disk_snap_script.ps1
@@ -3,9 +3,8 @@ Select-AzSubscription -SubscriptionName 'Free Trial'
 $rgName = 'Test-RG'
 $vmName = 'vmtest200'
 $location = 'eastus' 
-$snapshotName = 'vmtest200_OsDisk_snap1' 
+$snapshotName = 'vmtest200_OsDisk_snap2' 
 $vm = Get-AzVM -ResourceGroupName $rgName -Name $vmName
 Stop-AzVM -ResourceGroupName $rgName -Name $vmName
-$disk = Get-AzDisk -ResourceGroupName $rgName -Name $vmName -DiskName $vm.StorageProfile.OsDisk.Name
 $snapshot =  New-AzSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
 New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $rgName
