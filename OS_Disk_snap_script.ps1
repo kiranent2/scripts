3f2acf83ecaba486f3650979864dc983323a4c5a
kiranent2 <83274675+kiranent2@users.noreply.github.com>
2021-04-28 10:24:47 +0530

    Add files via upload

--- OS_Disk_snap_script.ps1
+++ OS_Disk_snap_script.ps1
@@ -4,7 +4,7 @@ $rgName = 'Test-RG'
 $vmName = 'vmtest50'
 $location = 'eastus' 
 $snapshotName = 'osdisksnap' 
-$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+$vm = Get-AzVM -ResourceGroupName $rgName -Name $vmName
 Stop-AzVM -ResourceGroupName $rgName -Name $vmName
 $snapshot =  New-AzSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
-New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName 
+New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $rgName
